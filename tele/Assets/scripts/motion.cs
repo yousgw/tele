f64a7ad348f31dc5b6c78e06a84a606a42ddd445
yousgw <r115322@g.kobe-kosen.ac.jp>
2020-03-11 23:37:19 +0900

    改善途中

--- tele/Assets/scripts/motion.cs
+++ tele/Assets/scripts/motion.cs
@@ -19,9 +19,9 @@ public class motion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (controller.isGrounded)
+        if (CheckGrounded())
         {
-            Debug.Log("Ground");
+            //Debug.Log("Ground");
             if (Input.GetKey(KeyCode.Space))
             {
                 GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 0) * 100);
@@ -40,5 +40,20 @@ public class motion : MonoBehaviour
             t_b_rb.AddForce(force);
         }
 
+    }
+
+    public bool CheckGrounded()
+    {
+        //CharacterControlle.IsGroundedがtrueならRaycastを使わずに判定終了
+        if (controller.isGrounded) { return true; }
+        //放つ光線の初期位置と姿勢
+        //若干身体にめり込ませた位置から発射しないと正しく判定できない時がある
+        var ray = new Ray(this.transform.position + Vector3.up * 0.1f, Vector3.down);
+        //探索距離
+        var tolerance = 0.55f;
+        int layerMask = ~(1 << 8);
+        //Raycastがhitするかどうかで判定
+        //地面にのみ衝突するようにレイヤを指定する
+        return Physics.Raycast(this.transform.position + Vector3.up * 0.1f, Vector3.down, tolerance, layerMask);
     }
 }
